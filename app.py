c89ef5e62212ee641be2a8728c4fdd96e0c56f0d
Agirunator <agirug@gmail.com>
2025-03-14 14:21:01 +0000

    Added nav bar and logout

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from click import confirm
 from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import sqlite3
 import bcrypt
 import re
@@ -8,13 +9,30 @@ import re
 def is_alphanumeric(s):
     return bool(re.match('^[a-zA-Z0-9]*$', s))
 
-app = Flask(__name__)
-app.secret_key = "b'a\x01\xe0I\x1dB\xd6\x1cGb\xc0:\xe4X\x8fV\xf2\xfb\xb9\xdf\xc0Ld\xbf\x0b<\xf8}-\xa1G\xce'"
-
 def get_db_connection():
     conn = sqlite3.connect("users.db")
     conn.row_factory = sqlite3.Row
     return conn
+
+app = Flask(__name__)
+
+app.secret_key = "b'a\x01\xe0I\x1dB\xd6\x1cGb\xc0:\xe4X\x8fV\xf2\xfb\xb9\xdf\xc0Ld\xbf\x0b<\xf8}-\xa1G\xce'"
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
+
+@login_manager.user_loader
+def load_user(user_id):
+    db = get_db_connection()
+    user_data = db.execute("SELECT id, username FROM users WHERE id = ?", (user_id,)).fetchone()
+    db.close()
+    if user_data:
+        return User(id=user_data['id'], username=user_data['username'])
+    return None
 
 @app.route('/')
 def index():
@@ -33,7 +51,7 @@ def login():
     password = request.form['password']
 
     db = get_db_connection()
-    db_user = db.execute("SELECT password FROM users WHERE username = ?", (username,)).fetchone()
+    db_user = db.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchone()
     db.close()
 
     if db_user is None:
@@ -43,6 +61,8 @@ def login():
     stored_password = db_user['password']
 
     if bcrypt.checkpw(password.encode('utf-8'), stored_password):
+        user = User(id=db_user['id'], username=db_user['username'])
+        login_user(user)
         return redirect(url_for('index'))
     
     flash("Invalid username or password", "error")
@@ -81,3 +101,9 @@ def register():
 
     flash("Registration successful! Please log in.", "success")
     return redirect(url_for('auth'))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))

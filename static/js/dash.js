dc31b8bf75d707b3b5588d3962e31575937fb991
Agirunator <agirug@gmail.com>
2025-03-15 23:38:39 +0000

    Added descriptions and links to pages using js

--- static/js/dash.js
+++ static/js/dash.js
@@ -4,15 +4,23 @@ boxes.forEach(box => {
     box.addEventListener('mouseenter', () => {
         boxes.forEach(otherBox => {
             if (otherBox !== box) {
-                otherBox.computedStyleMap.width = '25vw';
+                otherBox.style.width = '25vw';
         }
     });
-    box.computedStyleMap.width = '35vw';
+    box.style.width = '35vw';
     });
 
     box.addEventListener('mouseleave', () => {
         boxes.forEach(otherBox => {
-            otherBox.computedStyleMap.width = '30vw';
+            otherBox.style.width = '30vw';
         });
+    });
+});
+
+boxes.forEach(box => {
+    box.addEventListener('click', () => {
+        const url = box.getAttribute('data-href');
+        
+        window.location.href = url;
     });
 });
